fa2389a6dc7afe27adcd5a3501e0bd9e6c147652
josejuniorwm <josejuniorwm@gmail.com>
2022-07-20 14:56:40 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,10 +5,29 @@ import java.util.Scanner;
 
 import chess.ChessPiece;
 import chess.ChessPosition;
+import chess.Color;
 
 public class UI {
 	
-			// https://stackoverflow.com/questions/2979383/java-clear-the-console
+		public static final String ANSI_RESET = "\u001B[0m";
+		public static final String ANSI_BLACK = "\u001B[30m";
+		public static final String ANSI_RED = "\u001B[31m";
+		public static final String ANSI_GREEN = "\u001B[32m";
+		public static final String ANSI_YELLOW = "\u001B[33m";
+		public static final String ANSI_BLUE = "\u001B[34m";
+		public static final String ANSI_PURPLE = "\u001B[35m";
+		public static final String ANSI_CYAN = "\u001B[36m";
+		public static final String ANSI_WHITE = "\u001B[37m";
+	
+		public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+		public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+		public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+		public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+		public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+		public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+		public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+		public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";		
+	
 		public static void clearScreen() {
 			System.out.print("\033[H\033[2J");
 			System.out.flush();
@@ -31,7 +50,19 @@ public class UI {
 			for (int i=0; i<pieces.length; i++) {
 				System.out.print((8-i) + " ");
 				for (int j=0; j<pieces.length; j++){
-					printPiece(pieces[i] [j]);
+					printPiece(pieces[i] [j], false);
+				}
+				System.out.println();
+			}
+			
+			System.out.println("  a b c d e f g h");
+		}
+		
+		public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves) {
+			for (int i=0; i<pieces.length; i++) {
+				System.out.print((8-i) + " ");
+				for (int j=0; j<pieces.length; j++){
+					printPiece(pieces[i] [j], possibleMoves[i][j]);
 				}
 				System.out.println();
 			}
@@ -39,18 +70,22 @@ public class UI {
 			System.out.println("  a b c d e f g h");
 		}
 
-		private static void printPiece(ChessPiece piece) {
-			if (piece == null) {
-				System.out.print("-");
+		private static void printPiece(ChessPiece piece, boolean background) {
+			if (background) {
+				System.out.print(ANSI_BLUE_BACKGROUND);
 			}
-			else {
-					
-				 System.out.print(piece);
-				
-			}
-		    System.out.print(" ");
-			
-		
+	    	if (piece == null) {
+	            System.out.print("-" + ANSI_RESET);
+	        }
+	        else {
+	            if (piece.getColor() == Color.WHITE) {
+	                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+	            }
+	            else {
+	                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+	            }
+	        }
+	        System.out.print(" ");
 		}
 }
 
